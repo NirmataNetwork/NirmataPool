38aee80222fa25c683e62b1320340aed50547cfe
NwiZ <152017753+NirmataNetwork@users.noreply.github.com>
2024-01-20 10:41:39 +0300

    add

---
+++ website_example/config.js
@@ -0,0 +1,18 @@
+var api = "https://pool.nirmata-network.com/api";
+let parentCoin = "NiR"
+
+let byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH', 'EH', 'ZH', 'YH']
+
+var email = "nirmatanetwork@proton.me";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discord.gg/WbgKf5YZ";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.nirmata-network.com/{symbol}/block/{id}";
+//var blockchainExplorerMerged = "https://explorer.nirmata-network.com/?hash={id}#block";
+var transactionExplorer = "https://explorer.nirmata-network.com/{symbol}/transaction/{id}";
+//var transactionExplorerMerged = "https://explorer.nirmata-network.com/?hash={id}#transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
